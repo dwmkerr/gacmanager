fe7e834fb3a2830c30aa7108b5554d015390074f
Dave Kerr <dwmkerr@gmail.com>
2012-07-01 15:41:36 +0000

    Added the base code.

---
+++ GACManager/GACManager/GACManager/Models/IGACManagerModel.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using GACManagerApi;
+
+namespace GACManager.Models
+{
+    /// <summary>
+    /// The <see cref="IGACManagerModel"/> Model Interface class.
+    /// </summary>
+    public interface IGACManagerModel
+    {
+        //  Add functions to the model here.
+        TimeSpan EnumerateAssemblies(Action<AssemblyDetails> onAssemblyEnumerated);
+    }
+}
---
+++ GACManager/GACManager/GACManagerApi/AssemblyCacheEnumerator.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using GACManagerApi.Fusion;
+
+namespace GACManagerApi
+{
+    [ComVisible(false)]
+    public class AssemblyCacheEnumerator
+    {
+        public AssemblyCacheEnumerator()
+        {
+            Initialise(null);
+        }
+
+        public AssemblyCacheEnumerator(string assemblyName)
+        {
+            Initialise(assemblyName);
+        }
+
+        private void Initialise(string assemblyName)
+        {
+            IAssemblyName fusionName = null;
+            int hr = 0;
+    
+            //  If we have an assembly name, create the assembly name object.
+            if (assemblyName != null)
+            {
+                hr = FusionImports.CreateAssemblyNameObject(out fusionName, assemblyName,
+                    CreateAssemblyNameObjectFlags.CANOF_PARSE_DISPLAY_NAME, IntPtr.Zero);
+                if(hr < 0)
+                    Marshal.ThrowExceptionForHR(hr);
+            }
+
+                
+                hr = FusionImports.CreateAssemblyEnum(
+                    out assemblyEnumerator,
+                    IntPtr.Zero,
+                    fusionName,
+                    AssemblyCacheFlags.GAC,
+                    IntPtr.Zero);
+                if (hr < 0)
+                    Marshal.ThrowExceptionForHR(hr);
+
+        }
+
+        public AssemblyDetails GetNextAssembly()
+        {
+            int hr = 0;
+            IAssemblyName fusionName = null;
+
+            if (done)
+            {
+                return null;
+            }
+
+            // Now get next IAssemblyName from m_AssemblyEnum
+            hr = assemblyEnumerator.GetNextAssembly((IntPtr)0, out fusionName, 0);
+
+            if (hr < 0)
+            {
+                Marshal.ThrowExceptionForHR(hr);
+            }
+
+            if (fusionName != null)
+            {
+                var assemblyDetails = new AssemblyDetails();
+                assemblyDetails.Load(fusionName);
+                return assemblyDetails;
+            }
+            else
+            {
+                done = true;
+                return null;
+            }
+        }
+
+        private IAssemblyEnum assemblyEnumerator = null;
+        private bool done;
+    }
+}
