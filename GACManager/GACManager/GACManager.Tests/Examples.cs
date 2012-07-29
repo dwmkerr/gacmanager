83cffdbb1ec8a6f1654fda019200d56b5932d5ed
dwmkerr_cp <SND\dwmkerr_cp>
2012-07-29 12:32:06 +0000

    Version 1.0

---
+++ GACManager/GACManager/GACManager.Tests/Examples.cs
@@ -0,0 +1,146 @@
+﻿using System.Runtime.InteropServices;
+using GACManagerApi;
+using GACManagerApi.Fusion;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+
+namespace GACManager.Tests
+{
+    [TestClass()]
+    public class Examples
+    {
+        private TestContext testContextInstance;
+
+        /// <summary>
+        ///Gets or sets the test context which provides
+        ///information about and functionality for the current test run.
+        ///</summary>
+        public TestContext TestContext
+        {
+            get
+            {
+                return testContextInstance;
+            }
+            set
+            {
+                testContextInstance = value;
+            }
+        }
+
+        #region Additional test attributes
+        // 
+        //You can use the following additional attributes as you write your tests:
+        //
+        //Use ClassInitialize to run code before running the first test in the class
+        //[ClassInitialize()]
+        //public static void MyClassInitialize(TestContext testContext)
+        //{
+        //}
+        //
+        //Use ClassCleanup to run code after all tests in a class have run
+        //[ClassCleanup()]
+        //public static void MyClassCleanup()
+        //{
+        //}
+        //
+        //Use TestInitialize to run code before running each test
+        //[TestInitialize()]
+        //public void MyTestInitialize()
+        //{
+        //}
+        //
+        //Use TestCleanup to run code after each test has run
+        //[TestCleanup()]
+        //public void MyTestCleanup()
+        //{
+        //}
+        //
+        #endregion
+
+        /// <summary>
+        ///A test for CanFindExecutable
+        ///</summary>
+        [TestMethod()]
+        public void EnumerateAssemblies()
+        {
+            //  Create an assembly enumerator.
+            var assemblyEnumerator = new AssemblyCacheEnumerator();
+
+            //  Get the first assembly.
+            var assemblyName = assemblyEnumerator.GetNextAssembly();
+
+            //  Start to loop through the assemblies.
+            while(assemblyName != null)
+            {
+                //  The 'assemblyName' object is a COM interface, if we create an 
+                //  AssemblyDescription from it, we will have access to more information.
+                var assemblyDescription = new AssemblyDescription(assemblyName);
+
+                //  Show the display name.
+                System.Diagnostics.Trace.WriteLine("Display Name: " + assemblyDescription.DisplayName);
+
+                //  Move to the next assembly.
+                assemblyName = assemblyEnumerator.GetNextAssembly();
+            }
+        }
+
+        [TestMethod()]
+        public void InstallAssembly()
+        {
+            //  We'll need a path to the assembly to install.
+            var path = @"c:/MyAssembly.dll";
+
+            //  Install the assembly, without an install reference.
+            AssemblyCache.InstallAssembly(path, null, AssemblyCommitFlags.Default);
+        }
+
+        [TestMethod()]
+        public void UninstallAssembly()
+        {
+            //  We'll need a display name of the assembly to uninstall.
+            var displayName = @"Apex, Version=1.4.0.0, Culture=neutral, PublicKeyToken=98d06957926c086d, processorArchitecture=MSIL";
+
+            //  When we try to uninstall an assembly, an uninstall disposition will be
+            //  set to indicate the success of the operation.
+            var uninstallDisposition = IASSEMBLYCACHE_UNINSTALL_DISPOSITION.Unknown;
+
+            //  Install the assembly, without an install reference.
+            try
+            {
+                AssemblyCache.UninstallAssembly(displayName, null, out uninstallDisposition);
+            }
+            catch (Exception exception)
+            {
+                //  We've failed to uninstall the assembly.
+                throw new InvalidOperationException("Failed to uninstall the assembly.", exception);
+            }
+
+            //  Did we succeed?
+            if (uninstallDisposition == IASSEMBLYCACHE_UNINSTALL_DISPOSITION.IASSEMBLYCACHE_UNINSTALL_DISPOSITION_UNINSTALLED)
+            {
+                //  Hooray!   
+            }
+        }
+
+        [TestMethod()]
+        public void FusionProperties()
+        {
+            //  Get the first assembly in the global assembly cache.
+            var someAssembly = new AssemblyDescription(new AssemblyCacheEnumerator().GetNextAssembly());
+
+            //  Show the install references.
+            foreach(var installReference in someAssembly.FusionProperties.InstallReferences)
+                System.Diagnostics.Trace.WriteLine(installReference.Description);
+        }
+
+        [TestMethod()]
+        public void ReflectionProperties()
+        {
+            //  Get the first assembly in the global assembly cache.
+            var someAssembly = new AssemblyDescription(new AssemblyCacheEnumerator().GetNextAssembly());
+
+            //  Show the Runtime Version.
+            System.Diagnostics.Trace.WriteLine(someAssembly.ReflectionProperties.RuntimeVersion);
+        }
+    }
+}
--- GACManager/GACManager/GACManagerApi/AssemblyFusionProperties.cs
+++ GACManager/GACManager/GACManagerApi/AssemblyFusionProperties.cs
@@ -18,6 +18,10 @@ namespace GACManagerApi
             InstallReferences = new List<FUSION_INSTALL_REFERENCE>();
         }
 
+        /// <summary>
+        /// Loads the fusion properties given the assembly name COM object.
+        /// </summary>
+        /// <param name="assemblyName">Name of the assembly.</param>
         public void Load(IAssemblyName assemblyName)
         {
             //  Load the properties.
@@ -35,6 +39,11 @@ namespace GACManagerApi
                 InstallReferences.Add(reference);
                 reference = enumerator.GetNextReference();
             }
+
+            //  Load the reserved properties.
+            //ReservedHashValue = GetByteArrayProperty(assemblyName, ASM_NAME.ASM_NAME_HASH_VALUE);
+            //ReservedHashAlgorithmId = GetDwordProperty(assemblyName, ASM_NAME.ASM_NAME_HASH_ALGID);
+
         }
         internal UInt16 GetShortProperty(IAssemblyName name, ASM_NAME propertyName)
         {
@@ -45,6 +54,19 @@ namespace GACManagerApi
             byte high = Marshal.ReadByte(buffer, 1);
             Marshal.FreeHGlobal(buffer);
             return (UInt16)(low + (high << 8));
+        }
+
+        internal UInt32 GetDwordProperty(IAssemblyName name, ASM_NAME propertyName)
+        {
+            uint bufferSize = 512;
+            IntPtr buffer = Marshal.AllocHGlobal((int)bufferSize);
+            name.GetProperty(propertyName, buffer, ref bufferSize);
+            byte a = Marshal.ReadByte(buffer);
+            byte b = Marshal.ReadByte(buffer, 1);
+            byte c = Marshal.ReadByte(buffer);
+            byte d = Marshal.ReadByte(buffer, 1);
+            Marshal.FreeHGlobal(buffer);
+            return (UInt32)(a + (b << 8) + (c << 16) + (d << 24));
         }
 
         internal string GetStringProperty(IAssemblyName name, ASM_NAME propertyName)
@@ -110,6 +132,22 @@ namespace GACManagerApi
         /// The public key.
         /// </value>
         public byte[] PublicKey { get; set; }
+
+        /// <summary>
+        /// Gets or sets the hash value. This is currently reserved.
+        /// </summary>
+        /// <value>
+        /// The hash value.
+        /// </value>
+        public byte[] ReservedHashValue { get; set; }
+
+        /// <summary>
+        /// Gets or sets the reserved hash algorithm id.
+        /// </summary>
+        /// <value>
+        /// The reserved hash algorithm id.
+        /// </value>
+        public uint ReservedHashAlgorithmId { get; set; }
 
         /// <summary>
         /// Gets the install references.
